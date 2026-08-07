e55ba0042929d315f81f3f81ab6a6d0a48585e87
thewebpagebuilder <thewebpagebuilder@gmail.com>
2026-08-07 14:51:05 +0530

    chore: add Google Tag Manager snippets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PDTVS8WX');`
+          }}
+        />
         {googleVerification && (
           <meta name="google-site-verification" content={googleVerification} />
         )}
@@ -84,6 +93,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         )}
       </head>
       <body className="min-h-screen bg-pearl font-sans text-obsidian antialiased">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PDTVS8WX"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           <StorefrontChrome>{children}</StorefrontChrome>
         </Providers>
